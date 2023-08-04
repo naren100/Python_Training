f1656e19a0255d56fe04fbea695843ec1311ecc4
nareniyer <naren.iyer100@gmail.com>
2023-08-03 22:19:25 -0700

    Chapter 1 Commit

---
+++ Chapter 1.py
@@ -0,0 +1,31 @@
+
+# What is a Variable?
+# What is Explicit and Implicit Type
+# Python is a dynamically-typed language, which means the
+# A variable is determined automatically based on
+# the value assigned to it. You can simply assign a value to a variable,
+# and Python will figure out its type at runtime.
+# What is runtime vs Compile Time? - Homework
+# Naming and Using Variables
+# Properly name the variable
+# A traceback is a record of where the interpreter ran into trouble
+# when trying to execute your code.
+
+
+message = "Please do your goals"
+print(message)
+message = "Please do your goals on time"
+print(message)
+message = "This is a Test"
+print(mesage)
+
+# STRINGS
+# Anything inside quotes is a string
+name = "Dave Thomas"
+print(name)
+print(name.title())  # Title is a method. What is a method?
+print(name.upper())
+print(name.lower())
+
+
+
